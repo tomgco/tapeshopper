ab04dab83129a7dabb31219d4377ef78a6c0943f
Tom Gallacher <tomgallacher23@gmail.com>
2015-01-15 09:42:08 +0000

    Moving to tap parser

--- helloworld/index.js
+++ helloworld/index.js
@@ -2,7 +2,6 @@ var msee = require('msee');
 var path = require('path')
 var wrappedexec = require('../lib/wrapped-exec')
 var parser = require('tap-parser')
-var tap = require('tap')
 var verify = require('adventure-verify');
 
 exports.problem = msee.parseFile(__dirname + '/problem.md');
@@ -16,24 +15,17 @@ exports.verify = function (args, cb) {
   wrappedexec([ require('./wrap') ], args[0], function (ctx, done) {
     // we will need to wait for out module to finish running before we can do something.
     // but how can we do that?..
-    var tc = tap.createConsumer();
 
-    var rows = [];
-    tc.on('data', function (r) { rows.push(r) });
-    tc.on('end', function () {
-        var rs = rows.map(function (r) {
-            if (r && typeof r === 'object') {
-                return { id : r.id, ok : r.ok, name : r.name.trim() };
-            }
-            else return r;
-        });
+    var p = parser()
 
-        console.log(rs)
-        cb(false)
-
+    ctx.tape.createStream().on('data', function (chunk) {
+      p.write(chunk)
+    }).on('end', function () {
+      p.end()
+      console.log(p.results)
+      cb(false)
     })
 
-    ctx.tape.createStream().pipe(tc)
     done()
   })
 };
