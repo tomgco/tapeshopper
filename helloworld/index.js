7750c6286007c0b1b3cbc2dffb3913861e4684a9
Tom Gallacher <tomgallacher23@gmail.com>
2015-01-15 15:18:16 +0000

    Lets make verify tape-friendly ;]
    /superhack

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,8 +1,7 @@
 var msee = require('msee');
 var path = require('path')
-var wrappedexec = require('../lib/wrapped-exec')
-var parser = require('tap-parser')
-var verify = require('adventure-verify');
+var verify = require('../lib/tape/verify')
+var assert = require('assert')
 
 exports.problem = msee.parseFile(__dirname + '/problem.md');
 exports.solution = msee.parseFile(__dirname + '/solution.md');
@@ -11,21 +10,10 @@ exports.run = function (args) {
   var f = require(path.resolve(args[0]));
 };
 
-exports.verify = function (args, cb) {
-  wrappedexec([ require('./wrap') ], args[0], function (ctx, done) {
-    // we will need to wait for out module to finish running before we can do something.
-    // but how can we do that?..
-
-    var p = parser()
-
-    ctx.tape.createStream().on('data', function (chunk) {
-      p.write(chunk)
-    }).on('end', function () {
-      p.end()
-      console.log(p.results)
-      cb(false)
-    })
-
-    done()
-  })
-};
+exports.verify = verify(function (args, ctx) {
+  var calls = ctx.tapeCalls
+  assert(calls.plan >= 1, 'You have not called `t.plan(n)`')
+  assert.equal(calls.end, 1, 't.end should only be called once')
+  assert(calls.equal >= 1, 'You must have at least one call to `t.eqaul`')
+  assert.equal(ctx.tape.results.ok, false, 'Your tests should fail')
+});
